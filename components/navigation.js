94edf0c578a8458c78e5544408398e9404768db1
GGahagan27 <71200016+GGahagan27@users.noreply.github.com>
2022-04-02 09:02:53 -0800

    Renamed Pages, created JS Components for Events and Vinyl page. Deleted uneccessary files.

---
+++ components/navigation.js
@@ -0,0 +1,18 @@
+const navEl = document.getElementById("navigation");
+
+navEl.innerHTML = 'navigation'; //I'm not sure I'm doing this right. 
+<ul>
+<li><a href="index.html">Home</a> &nbsp;</li>
+<li><a href="events">Events</a> &nbsp;</li>
+<li><a href="new_used_records">New and Used Records</a> &nbsp;</li>
+<li><a href="skate_shop">Skate Shop</a> &nbsp;</li>
+<li><a href="Shop Moons">Shop Moon's</a>&nbsp;</li>
+<li><a href="about">about/contact</a>&nbsp;</li>
+<li><a href="https://nic-dgl103-f21-dlu.github.io/websiteproject-GGahagan27/index.html#about">About</a>&nbsp;</li>
+</ul>;
+
+// Create an Id for the navigation 
+// Delete the unordered list 
+// Rename pages in Nav list on JS file 
+
+//Make a new js. file for footer. Footer follows same conventions as above. 
